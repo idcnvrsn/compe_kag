0bac3e1de09989485b40184b5394087cfec851c7
idcnvrsn <idcnvrsn@gmail.com>
2019-03-10 20:47:28 +0900

    データが読み込めないのに対処。 デフォルトhpパラメータの導入。 ローカル変数を修正。

--- HouseholdElectricPowerConsumption/train_hp.py
+++ HouseholdElectricPowerConsumption/train_hp.py
@@ -22,6 +22,12 @@ from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
 from keras.callbacks import History
 
+hyperopt_parameters_def = {
+    'length_of_sequences':5,
+    'batch_size': 3,
+    'hidden_neurons': 100,
+}
+
 hyperopt_parameters = {
     'length_of_sequences' : hp.quniform('length_of_sequences',50, 150, 10),
     'batch_size': hp.quniform('batch_size', 2, 32, 2),
@@ -29,28 +35,30 @@ hyperopt_parameters = {
 #    'kernel': hp.choice('kernel', ['rbf', 'poly', 'sigmoid'])
 }
 
-
 def _load_data(data, n_prev = 30):  
     """
     data should be pd.DataFrame()
     """
     docX, docY = [], []
     for i in range(len(data)-n_prev):
-        docX.append(data.iloc[i:i+n_prev].as_matrix())
-        docY.append(data.iloc[i+n_prev].as_matrix())
+#        docX.append(data.iloc[i:i+n_prev].as_matrix())
+#        docY.append(data.iloc[i+n_prev].as_matrix())
+        docX.append(data.iloc[i:i+n_prev])
+        docY.append(data.iloc[i+n_prev])
     alsX = np.array(docX)
     alsY = np.array(docY)
 
     return alsX, alsY
 
-def train_test_split(df, test_size=0.1, n_prev = 100):  
+def train_test_split(_df, test_size=0.1, n_prev = 100):  
     """
     This just splits data to training and testing parts
     """
-    ntrn = round(len(df) * (1 - test_size))
+    ntrn = round(len(_df) * (1 - test_size))
     ntrn = int(ntrn)
-    X_train, y_train = _load_data(df.iloc[0:ntrn], n_prev)
-    X_test, y_test = _load_data(df.iloc[ntrn:], n_prev)
+
+    X_train, y_train = _load_data(_df.iloc[0:ntrn], n_prev)
+    X_test, y_test = _load_data(_df.iloc[ntrn:], n_prev)
 
     return (X_train, y_train), (X_test, y_test)
 
@@ -113,7 +121,7 @@ if __name__ == '__main__':
             # 最小化する値を定義した関数
             objective,
             # 探索するパラメータのdictもしくはlist
-            hyperopt_parameters,
+            hyperopt_parameters_def,
             # どのロジックを利用するか、基本的にはtpe.suggestでok
             algo=tpe.suggest,
             max_evals=trial_num,
